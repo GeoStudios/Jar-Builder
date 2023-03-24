2dee952412dfbce9a6bfc374a36973b0db4f203b
Zombi <zombeifedgaming@outlook.com>
2023-03-23 19:57:22 -0500

    main

--- go/main.go
+++ go/main.go
@@ -1,7 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+)
+
 func main() {
-
-	print("Hello World")
-
+	items, _ := ioutil.ReadDir(".")
+	for _, item := range items {
+		if item.IsDir() {
+			subitems, _ := ioutil.ReadDir(item.Name())
+			for _, subitem := range subitems {
+				if !subitem.IsDir() {
+					// handle file there
+					fmt.Println(item.Name() + "/" + subitem.Name())
+				}
+			}
+		} else {
+			// handle file there
+			fmt.Println(item.Name())
+		}
+	}
 }
