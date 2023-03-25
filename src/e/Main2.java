7e6949703ab09150c47588619a33aac08986be50
Zombii <93808177+ZombiiTheCoder@users.noreply.github.com>
2023-03-25 13:22:24 -0500

    Delete src directory

--- src/e/Main2.java
+++
@@ -1,8 +0,0 @@
-package e;
-
-public class Main2 {
-    public static void main(String[] args) {
-        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-        System.out.println(cars[0]);
-    }
-}
